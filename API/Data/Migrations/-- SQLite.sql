bed9ccd9eb4a24d8236b398eca6bc17afaa35dd3
khamwenhui <71804019+khamwenhui@users.noreply.github.com>
2020-10-10 22:15:11 +0800

    End of Section 3--

--- API/Data/Migrations/-- SQLite.sql
+++
@@ -1,9 +0,0 @@
--- SQLite
-INSERT INTO `Users` (Id, UserName)
-VALUES (1, "Aaron");
-
-INSERT INTO `Users` (Id, UserName)
-VALUES (2, "Ben");
-
-INSERT INTO `Users` (Id, UserName)
-VALUES (3, "Crystal");
